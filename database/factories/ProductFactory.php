0f8f0070bbd4d0240c1208cfbba90dcfdc0826de
Opportune Katché <katcheopportune@gmail.com>
2023-12-05 12:16:26 +0100

    seeding part for Product and Review

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use Faker\Generator as Faker ;
+
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ProductFactory extends Factory
@@ -11,10 +14,16 @@ class ProductFactory extends Factory
      *
      * @return array
      */
+
+   
     public function definition()
     {
         return [
-            //
+            'name'=>$this->faker->word,
+            'detail'=>$this->faker->paragraph,
+            'price'=>$this->faker->numberBetWeen(100, 1000),
+            'stock'=>$this->faker->randomDigit,
+            'discount'=>$this->faker->numberBetWeen(2, 30)
         ];
     }
 }
